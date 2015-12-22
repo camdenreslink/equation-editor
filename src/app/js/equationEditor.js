41a298ede89fbee448d7892c22e7fdcbb5a6ffd0
camdenre <camdenre@buffalo.edu>
2015-12-22 18:16:14 -0500

    Added a nicer loading spinner

--- src/app/js/equationEditor.js
+++ src/app/js/equationEditor.js
@@ -1,9 +1,39 @@
 var fontsLoaded = false;
 var imagesLoaded = false;
+var spinner = null;
+$(document).ready(function() {
+    var opts = {
+      lines: 9 // The number of lines to draw
+    , length: 12 // The length of each line
+    , width: 6 // The line thickness
+    , radius: 12 // The radius of the inner circle
+    , scale: 1 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.25 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '30%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+    };
+
+    var target = document.getElementById('loadingMessage');
+    spinner = new Spinner(opts).spin(target);
+});
 
 var setup = function() {
     if (fontsLoaded && imagesLoaded) {
-        $('.loadingMessage').remove();
+        spinner.stop();
+        $('#loadingMessage').remove();
+        $('#loadingMessageOuter').remove();
         initializePropertyHooks();
         setupKeyboardEvents();
         setupMenuEvents();
